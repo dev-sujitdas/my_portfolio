68d900f2972494016a24c876375daa5c076a9a5e
gravityglobalsolution <gravityglobalsolution@gmail.com>
2024-04-30 12:51:35 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// For Tabs In About Section
+
+function opentab(clickedTab, tabName) {
+    let tabLinks = document.querySelectorAll(".tab-links");
+    tabLinks.forEach((tabLink) => {
+        tabLink.classList.remove("active-link");
+    });
+
+    let tabContents = document.querySelectorAll(".tab-contents");
+    tabContents.forEach((tabContents) => {
+        tabContents.classList.remove("active-tabs");
+    });
+
+    clickedTab.classList.add("active-link");
+
+    let tabContent = document.getElementById(tabName);
+    tabContent.classList.add("active-tabs");
+}
+
+// For Colleting the Form Data In Email...
+const sendData = document.querySelector("#Send");
+sendData.addEventListener("click", function (e) {
+    
+    let nameVal = document.getElementById("Name").value;
+    let emailVal = document.getElementById("Email").value;
+    let messageVal = document.getElementById("Message").value;
+
+    let Val = nameVal + emailVal + messageVal;
+    
+    if(Val===""){
+        // alert("Please fill all the details..");
+    } else {
+        e.preventDefault();
+        sendMail();
+        document.getElementById("Name").value = "";
+        document.getElementById("Email").value = "";
+        document.getElementById("Message").value = "";
+    }    
+});
+
+function sendMail() {
+    const name = document.getElementById("Name").value;
+    const email = document.getElementById("Email").value;
+    const message = document.getElementById("Message").value;
+    const body = "Name: " + name + "<br/> Email: " + email + "<br/> Message: " + message;
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "dev.sujitdas@gmail.com",
+        Password: "6328C607AADC7C9738E41C430FE261173D54",
+        To: "dev.sujitdas@gmail.com",
+        From: "dev.sujitdas@gmail.com",
+        Subject: "New Enquiry From My Portfolio",
+        Body: body,
+    })    
+    .then(message =>{
+        if(message === 'OK'){
+            alert("Your submission was successful.");
+        } else {
+            alert("Someting went wrong. Please try again later.");
+        }
+    })    
+    .catch(error => {
+        console.error('There was an error!', error);
+        alert("Someting went wrong. Please try again later.");
+    }); 
+}
